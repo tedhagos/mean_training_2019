a7520912c64f8d7e82889b14a7a76ac1189ffd6b
Ted Hagos <tedhagos@gmail.com>
2019-11-06 10:19:44 +0800

    middleware example

--- temp/expressexample/app.js
+++ temp/expressexample/app.js
@@ -1,14 +1,33 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-const app = require('express')();
+// const app = require('express')();
 const home = require('./routes/home');
 const homePost = require('./routes/homePost');
 app.set('port', process.env.PORT || 3000);
 
+app.use('/', express.static('public'));
+app.use('/res2', express.static('img'));
+
+app.use('/author',(req, res, next) => {
+  let startTime = new Date();
+  res.on('finish', () => {
+    let endTime = new Date();
+    console.log(`Time elapsed : ${endTime - startTime}`);
+  });
+  next();
+});
 
 app.get('/', home.index );
 app.post('/', homePost.index);
+
+app.get('/book', (req, res) => {
+  res.send('BOOK page');
+});
+app.get('/book/:id', (req, res) => {
+  res.send(`Parameters passed : ${req.params.id}`);
+});
+
 
 
 app.listen(app.get('port'));
