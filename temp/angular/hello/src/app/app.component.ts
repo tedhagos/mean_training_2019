b5175c6ea65ef2ec584f369bc1b452e64a672ffe
Ted Hagos <tedhagos@gmail.com>
2019-11-07 11:43:33 +0800

    angular hello

---
+++ temp/angular/hello/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  lastname = "Doe";
+  firstname  = "John";
+  email = "johndoe@gmail.com";
+
+  favColor = "blue";
+
+  returnSomething() {
+    return "Something"
+  }
+
+  changeColor(tempcolor) {
+    this.favColor = tempcolor;
+  }
+
+  clickButton() {
+    this.firstname = "Jane";
+  }
+}
