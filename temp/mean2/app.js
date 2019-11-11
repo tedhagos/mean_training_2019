2f47189015c5c6eb32f0fedfa1b0045af11ba174
Ted Hagos <tedhagos@gmail.com>
2019-11-11 09:47:52 +0800

    commented on appjs dbUrl

--- temp/mean2/app.js
+++ temp/mean2/app.js
@@ -6,7 +6,12 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 app.use(cors())
 
+// MAKE SURE YOU REPLACE THIS ENTRY. The Url will be different for you
 const dbUrl = 'mongodb+srv://tedhagos:mongopassword@cluster0-jjekq.mongodb.net/books?retryWrites=true&w=majority'
+
+// IF YOU ARE RUNNING a local instance of mongoDb, you use this Url
+// const dbUrl = 'mongodb://localhost/books'
+
 const dbOptions = {useNewUrlParser:true, useUnifiedTopology: true}
 mongoose.connect(dbUrl, dbOptions)
 
